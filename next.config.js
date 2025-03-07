3980bfdfbe6701497df033318e45277c963592ea
Dowser <gabrielpestana37@gmail.com>
2025-03-07 18:42:48 -0300

    Fix: Corrige fonte e configuracoes do Next.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
   // Configuração para otimizar o build
-  output: 'standalone',
   experimental: {
     optimizeCss: true
   }
